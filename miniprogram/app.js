f11afd1d1fc2a2e2584c71f99a37addaf24dbf1d
Hansen1994 <646380243@qq.com>
2024-03-21 22:29:29 +0800

    feat:新增模态框、本地存储

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,1 +1,12 @@
-App({})
+// 执行 extendApi.js 文件，将方法挂载到 wx 全局对象身上
+import './utils/extendApi'
+
+App({
+
+
+  onShow() {
+    // console.log('1111111');
+    // wx.toast('数据啊数据')
+    wx.modal();
+  }
+})
